3771d4b245c5b0743223e992bcc3adc362eeaf78
Hafijul Islam <hafijul233@gmail.com>
2022-12-26 01:11:03 +0600

    old comments removed

--- app/src/main/java/com/hafijulislam/lemonade/MainActivity.kt
+++ app/src/main/java/com/hafijulislam/lemonade/MainActivity.kt
@@ -148,13 +148,6 @@ class MainActivity : AppCompatActivity() {
                 lemonText.setText(getString(R.string.lemon_empty_glass))
             }
         })
-
-        // TODO: for each state, the textAction TextView should be set to the corresponding string from
-        //  the string resources file. The strings are named to match the state
-
-        // TODO: Additionally, for each state, the lemonImage should be set to the corresponding
-        //  drawable from the drawable resources. The drawables have the same names as the strings
-        //  but remember that they are drawables, not strings.
     }
 
     /**
